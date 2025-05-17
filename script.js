9d01ab75d1be2a90a39c3ebb0437af0d324cbeef
wlcsv <wlcsv8080@outlook.com>
2025-05-17 12:17:07 -0300

    Criação da função exibeMensagem()

---
+++ script.js
@@ -0,0 +1,19 @@
+let contador = 0;
+
+/*
+
+Se o botão já tiver sido pressionado nada irá acontencer.
+
+*/
+
+function exibeMensagem() {
+  if (contador == 0 ) {
+    const botao = document.getElementById("botao");
+    const novoParagrafo = document.createElement("p");
+    novoParagrafo.textContent = "Foque apenas em sua arte (essa é minha filosofia de vida!)";
+    novoParagrafo.classList.add("novoParagrafo");
+    botao.insertAdjacentElement("afterend", novoParagrafo);
+
+    contador++;
+  } 
+} 
